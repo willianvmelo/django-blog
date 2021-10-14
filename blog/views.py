91ba6d9b153767fa0ccd4406261155d681752125
willianvmelo <willian.vieirademelo@gmail.com>
2021-10-14 11:52:50 -0300

    Alterando o layout da home.

--- blog/views.py
+++ blog/views.py
@@ -8,24 +8,10 @@ from django.core.mail import message, send_mail
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by= 2
+    paginate_by= 3
     template_name = 'blog/post/list.html'
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 2)  # três postagens em cada página
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # Se a página não for um inteiro, exibe a primeira página
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Se a página estiver fora do intervalo.
-        # Exibe a última página de resultados
-        posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
