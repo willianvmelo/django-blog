cd08188390a968c5fe9912940cf1f1fe3776684e
willianvmelo <willian.vieirademelo@gmail.com>
2021-11-10 11:07:44 -0300

    modificando a view post_detail para lidar com o formulario de comentarios

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post,Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm,CommentForm
 from django.core.mail import message, send_mail
 
 class PostListView(ListView):
@@ -17,6 +17,41 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',
                              publish__year=year, publish__month=month, publish__day=day)
+
+    # Obtendo a lista dos comentários ativos
+    comments = post.comments.filter(active = True) 
+    new_comment = None
+
+    if request.method == 'POST' :
+        # Um comentário foi postado
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+
+            # Cria o objeto Comment mas não o salva ainda no banco de dados.j
+
+            new_comment = comment_form.save(commit=False)
+
+            # Atribui a postagem atual ao comentário
+
+            new_comment.post = post
+
+            # salva o comentário no banco de dados
+
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+        return render(request,
+        'blog/post/detail.html',
+        {
+        'post' : post,
+        'comments' : comments,
+        'new_comment' : new_comment,
+        'comment_form' : comment_form
+
+        })        
+                             
     return render(request, 'blog/post/detail.html', {'post': post})
 
 # View para compartilhamento de posts via email
